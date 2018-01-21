54d59ba0a82ce90fb6c2afedbafa5142591ce0d9
jsafrit <jsafrit@truveon.com>
2018-01-21 01:30:26 -0500

    Week 3 - some fixes

--- exer03.py
+++ exer03.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+logfile = 'mini-access-log.txt'
+
 sample = '67.218.116.165 - - [30/Jan/2010:00:03:18 +0200] "GET /robots.txt HTTP/1.0" 200 99 "-" "Mozilla/5.0 (Twiceler-0.9 http://www.cuil.com/twiceler/robot.html)"'
 ip = '67.218.116.165'
 ts = '30/Jan/2010:00:03:18 +0200'
@@ -18,3 +20,18 @@ def parse_entry(line):
 
 if __name__ == '__main__':
     assert(parse_entry(sample) == (ip, ts, req))
+
+    logs = []
+
+    with open(logfile, 'r') as f:
+        while True:
+            line = f.readline().strip()
+            if not line:
+                break
+            # print(line)
+            ip_address, timestamp, request = parse_entry(line)
+            entry = {'ip_address': ip_address, 'timestamp': timestamp, 'request': request}
+            print(entry)
+            logs.append(entry)
+    print(len(logs))
+    print(logs[2])
