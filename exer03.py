a6cfb993f4c7fbe49385df35605ab5eeb2cd0a99
John Safrit <jsafrit@users.noreply.github.com>
2018-01-21 20:14:35 -0500

    More cleanup and fixed parser to use regex

--- exer03.py
+++ exer03.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import re
+
 logfile = 'mini-access-log.txt'
 
 sample = '67.218.116.165 - - [30/Jan/2010:00:03:18 +0200] "GET /robots.txt HTTP/1.0" 200 99 "-" "Mozilla/5.0 (Twiceler-0.9 http://www.cuil.com/twiceler/robot.html)"'
-ip = '67.218.116.165'
-ts = '30/Jan/2010:00:03:18 +0200'
-req = 'GET /robots.txt HTTP/1.0'
+test_ip = '67.218.116.165'
+test_ts = '30/Jan/2010:00:03:18 +0200'
+test_req = 'GET /robots.txt HTTP/1.0'
 
 
 def parse_entry(line):
@@ -19,27 +21,26 @@ def parse_entry(line):
 
 
 def parse_entry_re(line):
-    ip = line[:line.find(' ')]
-    ts = line[line.find('[')+1:line.find(']')]
+    search_pattern = r'(\d+.\d+.\d+.\d+).*\[(.*)\] "(.*?)"'
+    mo = re.search(search_pattern, line)
 
-    start = line.find('"') + 1
-    end = line.find('"', start)
-    req = line[start:end]
+    ip = mo.group(1)
+    ts = mo.group(2)
+    req = mo.group(3)
     return ip, ts, req
 
 
 if __name__ == '__main__':
-    assert parse_entry(sample) == (ip, ts, req)
-    assert parse_entry_re(sample) == (ip, ts, req)
+    assert parse_entry(sample) == (test_ip, test_ts, test_req)
+    assert parse_entry_re(sample) == (test_ip, test_ts, test_req)
 
     logs = []
-
     with open(logfile, 'r') as f:
         while True:
             single_entry = f.readline().strip()
             if not single_entry:
                 break
-            ip_address, timestamp, request = parse_entry(single_entry)
+            ip_address, timestamp, request = parse_entry_re(single_entry)
             entry = {'ip_address': ip_address, 'timestamp': timestamp, 'request': request}
             logs.append(entry)
 
